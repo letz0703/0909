b20eba705fbf0da29ae0705730968132dd15536e
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2022-12-13 21:07:18 +0900

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css"
 import Home from "./pages/Home"
 import Jap from "./pages/Jap"
 import Can from "./pages/Can"
-import NotFound from "./pages/Notfound"
+import NotFound from "./pages/NotFound"
 import Root from "./pages/Root"
 
 const router = createBrowserRouter([
