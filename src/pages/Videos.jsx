e689a49202593adc80090c8112af41e00c23ab92
ManI <letz0703@gmail.com>
2022-12-14 21:52:48 +0900

    video keyword or not?

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -3,5 +3,9 @@ import {useParams} from "react-router-dom"
 //import styles from './Videos.module.css'
 export default function Videos() {
   const {keyword} = useParams()
-  return <h1>Videos {keyword}</h1>
+  return (
+    <h1>
+      Videos {keyword ? `searched ${keyword}` : `no keyword so hot trend`}
+    </h1>
+  )
 }
