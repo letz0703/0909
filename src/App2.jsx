b0bc04a966433079c3eda649e3594833ecae6ac3
ManI <letz0703@gmail.com>
2022-12-15 08:55:17 +0900

    wip ...

--- src/App2.jsx
+++ src/App2.jsx
@@ -2,12 +2,16 @@ import {Outlet} from "react-router-dom"
 import SearchHeader from "./components/SearchHeader/SearchHeader"
 // import App from "./App"
 import styles from "./App2.module.css"
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   )
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,8 +1,18 @@
 import {useState, useEffect, useRef} from "react"
 import {useParams} from "react-router-dom"
 //import styles from './Videos.module.css'
+
 export default function Videos() {
   const {keyword} = useParams()
+  // move below under export default
+  const {
+    data: videos,
+    isLoading,
+    error
+  } = useQuery(["videos", keyword], async () => {
+    const res = await fetch(`/videos/${keyword ? "search" : "popular"}.json`)
+    return await res.json()
+  })
   return (
     <h1>
       Videos {keyword ? `searched ${keyword}` : `no keyword so hot trend`}
