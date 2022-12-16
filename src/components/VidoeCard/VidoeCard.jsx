af091b01406f14dbc1b4afea0ab283a04af2ad35
ManI <letz0703@gmail.com>
2022-12-16 15:46:13 +0900

    @VideoCard({video})
    from video.snippet

--- src/components/VidoeCard/VidoeCard.jsx
+++ src/components/VidoeCard/VidoeCard.jsx
@@ -1,5 +1,15 @@
-import {useState, useEffect, useRef} from "react"
+import {useState, useEffect, useRef} from 'react';
 //import styles from './VidoeCard.module.css'
 export default function VidoeCard({video}) {
-  return <div>{video.snippet.title}</div>
+  const {title, thumbnails, channelTitle, publishedAt} = video.snippet;
+  return (
+    <li>
+      <img src={thumbnails.medium.url} alt={title} />
+      <div>
+        <p>{title}</p>
+        <p>{channelTitle}</p>
+        <p>{publishedAt}</p>
+      </div>
+    </li>
+  );
 }
