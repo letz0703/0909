6f5fa7974968c3ee6b8c297929a590ed24effcb9
ManI <letz0703@gmail.com>
2022-12-18 20:25:39 +0900

    kdfjk

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,32 @@
 import {useState, useEffect} from "react"
 import {Link} from "react-router-dom"
 import {v4 as uuidv4} from "uuid"
+import {BsFillPencilFill} from "react-icons/bs"
+import {HiOutlineShoppingBag} from "react-icons/hi"
 //import styles from './Navbar.module.css'
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/jap">Jap</Link>
-      <Link to="/can">Can</Link>
-    </nav>
+    <header className="flex justify-between">
+      <Link to="/">
+        <div className="flex">
+          <img src="./src/assets/react.svg" />
+          <h1>canmart</h1>
+        </div>
+      </Link>
+      <nav className="flex items-center gap-4">
+        {/* <Link to="/">Home</Link> */}
+        <Link to="/jap">Jap</Link>
+        <Link to="/can">Can</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/carts">
+          <HiOutlineShoppingBag className="w-7 h-7" />
+        </Link>
+        <Link to="/product/new">
+          <BsFillPencilFill />
+        </Link>
+        <button>Login</button>
+      </nav>
+    </header>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 // import App from "./App"
-import App from "./App2"
+// import App from "./App2"
+import App from "./App3"
 import "./index.css"
+import AllProducts from "./pages/AllProducts"
+import MyCart from "./pages/MyCart"
+import NewProduct from "./pages/NewProduct"
 import NotFound from "./pages/NotFound"
+import ProductDetail from "./pages/ProductDetail"
+import ShopHome from "./pages/ShopHome"
 import VideoDetail from "./pages/VideoDetail"
 import Videos from "./pages/Videos"
 
@@ -14,7 +20,11 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <NotFound />,
     children: [
-      {index: true, element: <Videos />},
+      {index: true, element: <ShopHome />},
+      {path: "/products", element: <AllProducts />},
+      {path: "/products/new", element: <NewProduct />},
+      {path: "/products/:id", element: <ProductDetail />},
+      {path: "/carts", element: <MyCart />},
       {path: "/videos", element: <Videos />},
       {path: "/videos/:keyword", element: <Videos />},
       {path: "/videos/watch/:videoId", element: <VideoDetail />}
