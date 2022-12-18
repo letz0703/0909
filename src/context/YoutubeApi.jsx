36c457a0e5bef0f719be4ba74e461d5f508284a0
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2022-12-18 15:35:39 +0900

    deploy

--- src/context/YoutubeApi.jsx
+++ src/context/YoutubeApi.jsx
@@ -6,8 +6,8 @@ import YoutubeClient from "../api/youtubeClient"
 
 export const YoutubeApiContext = createContext()
 
-const client = new FakeYoutubeClient()
-// const client = new YoutubeClient()
+// const client = new FakeYoutubeClient()
+const client = new YoutubeClient()
 const youtube = new Youtube(client)
 
 export function YoutubeApiProvider({children}) {
