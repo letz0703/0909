fd856427ff47aa98d0847b73ea26f787a31bde8b
ManI <letz0703@gmail.com>
2022-12-22 22:10:08 +0900

    Banner Setup

---
+++ src/components/banners/banners.jsx
@@ -0,0 +1,5 @@
+import {useState, useEffect, useRef} from "react"
+//import styles from './banners.module.css'
+export default function Banners() {
+  return <h1>Banners</h1>
+}
--- src/pages/ShopHome.jsx
+++ src/pages/ShopHome.jsx
@@ -1,5 +1,12 @@
 import {useState, useEffect, useRef} from "react"
+import Banners from "../components/banners/banners"
+import Products from "../components/Products"
 //import styles from './ShopHome.module.css'
 export default function ShopHome() {
-  return <h1>ShopHome</h1>
+  return (
+    <>
+      <Banners />
+      <Products />
+    </>
+  )
 }
