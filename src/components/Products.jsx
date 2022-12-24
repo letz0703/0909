73422b4732d3f14917e24307a45670781a7d2d3c
ManI <letz0703@gmail.com>
2022-12-25 03:40:58 +0900

    ...

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -10,6 +10,7 @@ export default function Products() {
     <>
       {isLoading && <p>Loading...</p>}
       {error && <p>{error}</p>}
+      <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         {products &&
           products.map(product => (
             <ProductCard key={product.id} product={product} />
