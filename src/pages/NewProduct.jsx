331eec7548bb024c3df7d12929403ac556a6b0e3
ManI <letz0703@gmail.com>
2023-01-16 07:52:58 +0900

    new design

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -10,6 +10,7 @@ export default function Customs() {
   const [name, setName] = useState("")
   const [price, setPrice] = useState(0)
   const [imgUrl, setImgUrl] = useState("")
+  const [stock, setStock] = useState(0)
   // const { user, uid } = useAuthContext()
   const japitemRef = collection(db, "japitem") //customNo, userId
   const [japitems, setJapitems] = useState([])
@@ -20,6 +21,7 @@ export default function Customs() {
       name: name,
       price: price,
       imgUrl: imgUrl,
+      stock: stock,
     })
   }
   useEffect(() => {
@@ -31,41 +33,63 @@ export default function Customs() {
   }, [])
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="아이템코드"
-        onChange={(e) => setCode(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="item name"
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="number"
-        placeholder="price"
-        onChange={(e) => setPrice(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="imgUrl"
-        onChange={(e) => setImgUrl(e.target.value)}
-      />
+    <div className="flex flex-col justify-center items-center">
+      <form
+        className="flex flex-col justify-center text-center"
+        style={{ maxWidth: "300px" }}
+      >
+        <input
+          type="text"
+          placeholder="아이템코드"
+          onChange={(e) => setCode(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="item name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="price"
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="imgUrl"
+          onChange={(e) => setImgUrl(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="stock"
+          step={5}
+          onChange={(e) => setStock(e.target.value)}
+        />
+      </form>
       <button className="btn btn--primary" onClick={createJapitem}>
         아이템등록
       </button>
       {japitems.map((japitem) => (
-        <div
-          key={uuidv4()}
-          className="new-product__list place-content-center text-center"
-        >
-          <span>code: {japitem.code}</span>
-          <span>{japitem.name}</span>
-          <span>{FormatCurrency(japitem.price)}</span>
-          <span>
-            <img src={japitem.imgUrl} className="new-product__list-image" />
-          </span>
+        <div key={uuidv4()}>
+          <div className="new-product__list place-content-center text-center">
+            <span>code: {japitem.code}</span>
+            <span>{japitem.name}</span>
+            <span className="text-orange-500 font-bold">
+              {FormatCurrency(japitem.price)}
+            </span>
+
+            <span>
+              <img src={japitem.imgUrl} className="new-product__list-image" />
+            </span>
+          </div>
+          <div className="new-product__list place-content-center">
+            <span>&times;</span>
+            <span>
+              {japitem.stock}개{" "}
+              <span className="text-xs text-gray-500">in stock</span>
+            </span>
+            <span className="font-semibold">TOTAL: </span>
+            <span>{FormatCurrency(japitem.stock * japitem.price)}</span>
+          </div>
 
           <style>{`
           .new-product__list{
