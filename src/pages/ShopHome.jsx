fcf0c481ad31916d75304edab0357f9d41e837a4
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2023-01-15 13:10:31 +0900

    ...

--- src/pages/ShopHome.jsx
+++ src/pages/ShopHome.jsx
@@ -1,16 +1,20 @@
 import { useState, useEffect, useRef, useContext } from "react"
 import { JapitemContext, SearchContext, TestContext } from "../App"
 import Banners from "../components/banners/banners"
+import Orders from "../components/orders/orders"
 import Products from "../components/Products"
 import SearchData from "../components/SearchData"
+import { v4 as uuidv4 } from "uuid"
+import { useAuthContext } from "../context/AuthContext"
 //import styles from './ShopHome.module.css'
+
 export default function ShopHome({ search }) {
-  const { japitems } = useContext(JapitemContext)
   return (
     <>
-      <Banners />
+      <Orders />
+      {/* <Banners /> */}
       {/* <Products /> */}
-      <SearchData japitems={japitems} />
+      {/* <SearchData japitems={japitems} /> */}
     </>
   )
 }
