a4cc0c7ca9d07286876a5d9c2558ca50b1dc3dd7
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2023-01-31 13:45:17 +0900

    before navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,123 +20,80 @@ export default function Navbar({ search, setSearch }) {
   const { handleJapitemSearch } = useContext(JapitemContext)
   const navigate = useNavigate()
   return (
-    <div className={`${styles.icNavbar} ${styles.icNavbar__center}`}>
-      {/* <Row> */}
-      <header sticky="top">
-        <section className="icNavbar__sec1">
-          {/* <div className=" text-brand"> */}
-          <div className="flex items-center align-items-center">
-            <div>
-              {user && user.isAdmin ? (
-                <Link to="/products/new" className="2xl">
-                  <BsFillPencilFill />
-                </Link>
-              ) : (
-                <span
-                  style={{
-                    fontSize: "1.3rem",
-                    paddingRight: ".1rem",
-                    fontWeight: "bold",
-                    color: "black",
-                  }}
-                >
-                  i.
-                </span>
-              )}
-            </div>
-            <div>
-              <Link to="/" className="flex items-center text-4xl">
-                <span style={{ fontSize: "1.4rem", color: "black" }}>
-                  canmart
-                </span>
-              </Link>
-            </div>
-            {user && (
-              <Button
-                text={"logout"}
-                onClick={logout}
-                style={{ point: "cursor" }}
-              />
-            )}
-            {!user && <Button text={"login"} onClick={login} />}
-          </div>
-          {/* </div> */}
-
+    <header className=" flex flex-1 justify-between  p-4 ">
+      <section className="icNavbar__sec1">
+        {/* <div className=" text-brand"> */}
+        <div className="flex items-center align-items-center">
           <div>
             {user && user.isAdmin ? (
-              <BsYoutube
-                className="navbar__youtube-icon fs-4"
-                onClick={() => window.location.replace("/videos")}
-              />
+              <Link to="/products/new" className="2xl">
+                <BsFillPencilFill />
+              </Link>
             ) : (
-              ""
+              <span
+                style={{
+                  fontSize: "1.3rem",
+                  paddingRight: ".1rem",
+                  fontWeight: "bold",
+                  color: "black",
+                }}
+              >
+                i.
+              </span>
             )}
           </div>
-          {/* <Link to="/videos"> */}
-        </section>
-        {!window.location.href.includes("/jap") && (
-          <section className="sec2">
-            <div className={`${styles.nav_center} navbar__search-input`}>
-              <SearchInput setSearch={setSearch} />
-            </div>
-          </section>
-        )}
-
-        {/* <form>
-        <input
-          type="text"
-          onChange={(e) => setSearch(e.target.value)}
-          placeholder="🔎 search..."
-          autoFocus
-          className="navbar__input text-center"
-        />
-      </form> */}
-
-        <section className="sec3">
           <div>
-            <NavbarBs className="ic-navbar" sticky="top">
-              <div className="flex justify-center items-center gap-3 me-auto">
-                {/* <Link to="/can">Can</Link> */}
-                <Link to="/jap">Jap</Link>
-                <Link to="/shop">Store</Link>
-                {user && <User user={user} />}
-                {/* {!user && <Button text={"login"} onClick={login} />} */}
-                {/* {user && (
-                    <Button
-                      text={"logout"}
-                      onClick={logout}
-                      style={{ point: "cursor" }}
-                    />
-                  )} */}
-              </div>
-              {user && (
-                <Link to="/carts">
-                  <CartStatus />
-                </Link>
-              )}
-            </NavbarBs>
+            <Link to="/" className="flex items-center text-4xl">
+              <span style={{ fontSize: "1.4rem", color: "black" }}>
+                canmart
+              </span>
+            </Link>
+          </div>
+          {user && (
+            <Button
+              text={"logout"}
+              onClick={logout}
+              style={{ point: "cursor" }}
+            />
+          )}
+          {!user && <Button text={"login"} onClick={login} />}
+        </div>
+
+        <div>
+          {user && user.isAdmin ? (
+            <BsYoutube
+              className="navbar__youtube-icon fs-4"
+              onClick={() => window.location.replace("/videos")}
+            />
+          ) : (
+            ""
+          )}
+        </div>
+      </section>
+      {!window.location.href.includes("/jap") && (
+        <section className="sec2">
+          <div className={`${styles.nav_center} navbar__search-input`}>
+            <SearchInput setSearch={setSearch} />
           </div>
         </section>
-
-        {/* <style>{`
-        .navbar {
-          display: flex;
-          justify-content: space-evenly;
-          align-items: center;
-        }
-
-      .navbar__header {
-            display: flex;
-            align-items: center;
-      }
-      .navbar__input {
-        font-size: 2rem;
-        border-bottom: 1px dotted #C10002;
-      }
-
-      `}</style> */}
-      </header>
-      {/* </Row> */}
-    </div>
+      )}
+      <section className="sec3">
+        <div>
+          <NavbarBs className="ic-navbar" sticky="top">
+            <div className="flex justify-center items-center gap-3 me-auto">
+              {/* <Link to="/can">Can</Link> */}
+              <Link to="/jap">Jap</Link>
+              <Link to="/shop">Store</Link>
+              {user && <User user={user} />}
+            </div>
+            {user && (
+              <Link to="/carts">
+                <CartStatus />
+              </Link>
+            )}
+          </NavbarBs>
+        </div>
+      </section>
+    </header>
   )
 }
