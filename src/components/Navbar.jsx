693d327d4fa0ccae3da35250764811a99f867546
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2023-01-31 15:39:19 +0900

    git push 전까지

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Nav, Navbar as NavbarBs, Col, Row } from "react-bootstrap"
 import { Link, useNavigate } from "react-router-dom"
 import { BsFillPencilFill } from "react-icons/bs"
+import { HiLogin, HiLogout } from "react-icons/hi"
 import { login, logout, onUserStateChange } from "../api/firebase"
 import Button from "./ui/button"
 import User from "./User"
@@ -20,56 +21,65 @@ export default function Navbar({ search, setSearch }) {
   const { handleJapitemSearch } = useContext(JapitemContext)
   const navigate = useNavigate()
   return (
-    <header className=" flex flex-1 justify-between  p-4 ">
-      <section className="icNavbar__sec1">
-        {/* <div className=" text-brand"> */}
-        <div className="flex items-center align-items-center">
-          <div>
-            {user && user.isAdmin ? (
-              <Link to="/products/new" className="2xl">
-                <BsFillPencilFill />
-              </Link>
-            ) : (
-              <span
-                style={{
-                  fontSize: "1.3rem",
-                  paddingRight: ".1rem",
-                  fontWeight: "bold",
-                  color: "black",
-                }}
-              >
-                i.
-              </span>
-            )}
-          </div>
-          <div>
-            <Link to="/" className="flex items-center text-4xl">
-              <span style={{ fontSize: "1.4rem", color: "black" }}>
-                canmart
-              </span>
-            </Link>
-          </div>
-          {user && (
-            <Button
-              text={"logout"}
-              onClick={logout}
-              style={{ point: "cursor" }}
-            />
-          )}
-          {!user && <Button text={"login"} onClick={login} />}
+    <header className=" flex border-b justify-between p-3">
+      {/* <section className="icNavbar__sec1"> */}
+      {/* <div className=" text-brand"> */}
+      <div className="flex items-center align-items-center">
+        {user && user.isAdmin ? (
+          <Link to="/products/new" className="2xl pr-1">
+            <BsFillPencilFill />
+          </Link>
+        ) : (
+          <span
+            style={{
+              fontSize: "1.3rem",
+              paddingRight: ".1rem",
+              fontWeight: "bold",
+              color: "black",
+            }}
+          >
+            i.
+          </span>
+        )}
+        <div>
+          <Link to="/" className="flex items-center text-2xl">
+            canmart
+          </Link>
         </div>
 
-        <div>
-          {user && user.isAdmin ? (
-            <BsYoutube
-              className="navbar__youtube-icon fs-4"
-              onClick={() => window.location.replace("/videos")}
-            />
-          ) : (
-            ""
-          )}
-        </div>
-      </section>
+        {user && <User user={user} />}
+        {user && (
+          <span onClick={logout} className="text-xs p-1 bg-black text-white">
+            logout
+          </span>
+          // <Button
+          //   text={"logout"}
+          //   onClick={logout}
+          //   style={{ point: "cursor" }}
+          // />
+        )}
+        {!user && (
+          <span onClick={login}>login</span>
+          // <button
+          //   text={"로그인"}
+          //   onClick={login}
+          //   className="btn btn--primary mini"
+          // />
+        )}
+        {/* </div> */}
+
+        {/* <div> */}
+        {user && user.isAdmin ? (
+          <BsYoutube
+            className="navbar__youtube-icon fs-4 pl-2"
+            style={{ width: "100%", color: "red" }}
+            onClick={() => window.location.replace("/videos")}
+          />
+        ) : (
+          ""
+        )}
+      </div>
+      {/* </section> */}
       {!window.location.href.includes("/jap") && (
         <section className="sec2">
           <div className={`${styles.nav_center} navbar__search-input`}>
@@ -77,23 +87,20 @@ export default function Navbar({ search, setSearch }) {
           </div>
         </section>
       )}
-      <section className="sec3">
-        <div>
-          <NavbarBs className="ic-navbar" sticky="top">
-            <div className="flex justify-center items-center gap-3 me-auto">
-              {/* <Link to="/can">Can</Link> */}
-              <Link to="/jap">Jap</Link>
-              <Link to="/shop">Store</Link>
-              {user && <User user={user} />}
-            </div>
-            {user && (
-              <Link to="/carts">
-                <CartStatus />
-              </Link>
-            )}
-          </NavbarBs>
+      {/* <section className="sec3"> */}
+      <nav className="flex item-center">
+        <div className="flex justify-center items-center gap-3 me-auto">
+          {/* <Link to="/can">Can</Link> */}
+          <Link to="/jap">Jap</Link>
+          <Link to="/shop">Store</Link>
+          {user && (
+            <Link to="/carts">
+              <CartStatus />
+            </Link>
+          )}
         </div>
-      </section>
+      </nav>
+      {/* </section> */}
     </header>
   )
 }
