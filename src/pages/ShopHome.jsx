af222100540ac6a30f46f6992d3231f612cf1636
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2023-02-02 12:20:38 +0900

    검색 시 배너 안보이게 하기

--- src/pages/ShopHome.jsx
+++ src/pages/ShopHome.jsx
@@ -1,3 +1,5 @@
+import { useContext } from "react"
+import { SearchContext } from "../App"
 import JorderQuery from "../components/jorder-special/jorder-special"
 import Products from "../components/Products"
 import SearchInput from "../components/search-input/search-input"
@@ -5,9 +7,10 @@ import Slide from "../components/slide/slide"
 import Store from "./Store"
 
 export default function ShopHome() {
+  const { search } = useContext(SearchContext)
   return (
     <div className="shopHome">
-      <Slide />
+      {!search && <Slide />}
       <Products />
       {/* <style>{`
         .shopHome{
