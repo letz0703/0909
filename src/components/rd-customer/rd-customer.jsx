881dda248a4c04696ca8aabfd8a61d3b647521f4
ManI <letz0703@gmail.com>
2023-02-05 22:14:27 +0900

    통관번호 입력 ok

--- src/components/rd-customer/rd-customer.jsx
+++ src/components/rd-customer/rd-customer.jsx
@@ -1,13 +1,14 @@
 /**
  * 1 Make rd-customer database
- * 2 input for uid and cell and create rdb
- * 3 read from rdb - customers
- * ! rdb add for customNo
+ * 2 input for uid and cell and createdatabase,`customers/${data.uid}
+` * 3 read from database,`customers/${data.uid}` - customers
+ * ! database,`customers/${data.uid}` add for customNo
  *
  */
 import { database } from "../../api/firebase"
 import { useState, useEffect, useRef } from "react"
 import { useAuthContext } from "../../context/AuthContext"
+import styles from "./rd-customer.module.css"
 import {
   getDatabase,
   ref,
@@ -27,51 +28,64 @@ export default function RdCustomer({ _notice }) {
   })
 
   const { user } = useAuthContext()
-  const [customer, setCustomer] = useState([])
-  const [cell, setCell] = useState("")
-  const [customNo, setCustomNo] = useState("")
+  // const [customer, setCustomer] = useState([])
+  // const [cell, setCell] = useState("")
+  // const [customNo, setCustomNo] = useState("")
+
+  useEffect(() => {
+    user && read_rdb_customer()
+  }, [user])
 
   function getAllInputs() {
-    return {
-      uid: user?.uid,
-      cell: (state.cell && state.cell) || "",
-      name: user.displayName,
-      customNo: (state.customNo && state.customNo) || "",
+    if (!user) {
+      alert("login!")
+    } else {
+      return {
+        uid: user?.uid,
+        cell: (state.cell && state.cell) || "",
+        name: user.displayName,
+        customNo: (state.customNo && state.customNo) || "",
+      }
     }
   }
   const create_rdb_customers = (e) => {
     const data = getAllInputs()
 
-    !user
-      ? alert("login!")
-      : set(ref(database, `customers/${data.uid}`), {
-          name: user.displayName,
-          uid: user.uid,
-          cell: state.cell,
-          customNo: data.customNo,
-        })
-          .then(() => alert("data created"))
-          .catch((error) => console.log(error))
+    user &&
+      set(ref(database, `customers/${data.uid}`), {
+        name: user?.displayName,
+        uid: user?.uid,
+        cell: Number(state.cell),
+        customNo: data.customNo,
+      })
+        .then(() => alert("data saved"))
+        .catch((error) => console.log(error))
+  }
+
+  function delete_rdb_customer() {
+    const data = getAllInputs()
+    remove(ref(database, `customers/${data.uid}`))
+      .then(() => {
+        alert("data removed")
+      })
+      .catch((error) => console.log(error))
   }
 
   const read_rdb_customer = () => {
     get(child(ref(database), `customers/${user?.uid}`)) //
       .then((snapshot) => {
         if (snapshot.exists()) {
-          setCustomer(snapshot.val())
-        } else {
-          alert("no data")
+          setState({
+            uid: snapshot.val().uid,
+            cell: snapshot.val().cell,
+            customNo: snapshot.val().customNo || "",
+          })
         }
-        console.log(snapshot.val())
+        // else {
+        //   alert("no data")
+        // }
       })
       .catch((error) => alert(error))
-  }
-
-  function handleChange_cell(e) {
-    setCell(e.target.value)
-  }
-  function handleChange_customNo(e) {
-    setCustomNo(e.target.value)
   }
 
   const handleSubmit = (e) => {
@@ -79,45 +93,55 @@ export default function RdCustomer({ _notice }) {
     const id = e.target.id
     if (id == "btnCreate") {
       create_rdb_customers()
+    } else if (id == "btnSelect") {
+      read_rdb_customer()
+    } else if (id == "btnDelete") {
+      delete_rdb_customer()
     }
-    // else if (id == "btnSelect") {
-    // read_rdb_customer()
-    // }
   }
 
   return (
     <div>
       <form>
-        {console.log(state)}
-        <label>phone : </label>
+        <label>phone</label>
         <input
           type="text"
           id="userbox"
           value={state.cell}
-          // value={state.cell ? state.cell : ""}
-          // placeholder={customer.cell || ""}
-          // value={customer.cell || ""}
           onChange={(e) => {
-            setState({ cell: e.target.value })
+            setState({ ...state, cell: e.target.value })
           }}
+          className="bg-slate-300"
         />
-        <br />
-        <br />
-        {/* <label htmlFor="customNo">개인통관 번호 : </label>
+        <label>개인통관 번호</label>
         <input
           type="text"
-          id="customNo"
-          placeholder={customer.customNo || ""}
-          onChange={handleChange_customNo}
-        /> */}
+          id="userbox"
+          value={state.customNo}
+          onChange={(e) => {
+            setState({ ...state, customNo: e.target.value })
+          }}
+          className="bg-slate-300"
+        />
         <button
           id="btnCreate"
           type="submit"
           className="btn btn--primary"
           onClick={handleSubmit}
         >
-          write
+          save
         </button>
+        {user && user.isAdmin && (
+          <button
+            id="btnDelete"
+            type="submit"
+            className="btn btn--primary"
+            onClick={handleSubmit}
+          >
+            통관정보삭제
+          </button>
+        )}
+
         {/* <button
           id="btnSelect"
           className="btn btn--primary mini"
