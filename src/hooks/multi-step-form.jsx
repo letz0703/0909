340ed60bad0ed5cddd68c816074c726f57a96b62
ManI <letz0703@gmail.com>
2023-02-09 07:33:16 +0900

    hook : multi-step-form

---
+++ src/hooks/multi-step-form.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react"
+
+export function multiStepForm(steps) {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+
+  function next() {
+    setCurrentStepIndex((i) => {
+      if (i >= steps.length - 1) return i /** if last page */
+      return i + 1
+    })
+    return
+  }
+  function back() {
+    setCurrentStepIndex((i) => {
+      if (i <= 0) return i
+      return i - 1
+    })
+    return
+  }
+
+  function goto(index) {
+    setCurrentStepIndex(index)
+  }
+
+  function name() {
+    return
+  }
+
+  function name() {}
+  return { currentStepIndex, step: steps[currentStepIndex], goto, next, back }
+}
