98486a50910bc1974527dc7d4b2a989b6f53a290
ManI <letz0703@gmail.com>
2023-02-17 22:10:51 +0900

    데이터 베이스 id 추가

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,9 +1,7 @@
 import { useContext } from "react"
-import { Col, Row } from "react-bootstrap"
 import { SearchContext } from "../App"
-// import storeItems from "../data/items.json"
+import Products from "../components/Products"
 import { useJapitems } from "../hooks/use-japitems"
-import StoreItem from "./Store__Item"
 
 export default function Store() {
   const { search } = useContext(SearchContext)
@@ -11,21 +9,7 @@ export default function Store() {
   return (
     <>
       <h1>Store</h1>
-      <Row xs={1} md={2} lg={4} xl={5} className="g-3">
-        {/* <div xs={1} md={2} lg={4} xl={5} className="g-3"> */}
-        {japitems
-          .filter((item) => {
-            return search.toLowerCase() === ""
-              ? item
-              : item.name.toLowerCase().includes(search)
-          })
-          .map((japitem) => (
-            // <Col key={japitem}>{JSON.stringify(japitem.products)}</Col>
-            <Col key={japitem.id}>
-              <StoreItem {...japitem} />
-            </Col>
-          ))}
-      </Row>
+      <Products />
     </>
   )
 }
