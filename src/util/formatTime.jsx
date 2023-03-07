72a8d260154d59cde6805cc351a0e423c239755b
ManI <letz0703@gmail.com>
2023-03-08 08:04:11 +0900

    2023.03.08 am

--- src/util/formatTime.jsx
+++ src/util/formatTime.jsx
@@ -7,7 +7,8 @@ const getCurrentTime = () => {
 
 export default function FormatTIME(time) {
   var m = moment().tz("Asia/Seoul") // ← 이곳이 포인트
-  return m.format("ddd, YY/MM/DD HH:mm")
+  return m.format("YY/MM/DD, ddd")
+  // return m.format("ddd, YY/MM/DD HH:mm")
   // return m.format("YYYY-MM-DD HH:mm:ss")
   // console.log(getCurrentTime())	// return m.format("YYYY-MM-DD HH:mm")
 }
