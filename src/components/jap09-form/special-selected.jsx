2256ad14e32afbe878f841461996999febbf05c9
ManI <letz0703@gmail.com>
2023-03-13 03:04:18 +0900

    통관 상세 안내

--- src/components/jap09-form/special-selected.jsx
+++ src/components/jap09-form/special-selected.jsx
@@ -5,16 +5,16 @@ export default function SpecialSelected({ updateFields }) {
 
   return (
     <div className="bg-blue-100 p-4 rounded-b-xl">
-      <FormWrapper title="추가 주문 안내"></FormWrapper>
-      <div>공동구매 상품은 정해진 수량만 주문 할 수 있습니다</div>
-      <br />
-      <div class="extra-form__info">
-        <div>
-          추가로 주문을 원하시면 store 메뉴에서 깡통시장 구매대행을 이용해
-          주세요
+      <FormWrapper title="추가 주문 안내">
+        <div className="bg-red-300 p-2">
+          공동구매 상품은 정해진 수량만 주문 할 수 있습니다
         </div>
-        <div>공동구매 신청시 깡통시장 상품의 기본 배송료는 무료 입니다.</div>
-      </div>
+        <div class="extra-form__info">
+          <div>오프라인 상품 추가 가능하며</div>
+          <div>공동구매 신청시 기본 배송료는 무료 입니다.</div>
+        </div>
+      </FormWrapper>
+      <br />
       <style>{`
         .extra-from__info {
           padding-top: 1rem;
