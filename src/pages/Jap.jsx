6ed8387b1c3a999fae9b82a661c5f06665d1a124
ManI <letz0703@gmail.com>
2023-03-12 10:29:01 +0900

    해외 공동구매 안내

--- src/pages/Jap.jsx
+++ src/pages/Jap.jsx
@@ -66,37 +66,47 @@ export default function Jap() {
     }
   }
 
+  const comment =
+    "깡통시장 물건을 먼저 보내드립니다. 고객님의 통관 번호로 공동 구매합니다.만원 할인된 가격및 무료배송 혜택. 통관되는 물건은 저희 소유 입니다.입금 후 10일간 해외주문 금지"
+  const arr_jap_order_info = comment.split(".")
+
   return (
     <div className="jap__ flex flex-col align-items-start justify-start">
       <div className="jap__primary-header">
-        <div className="w-full h-full align-top bg-pink-100">
-          <h1 className="pt-3">해외 공동구매 안내</h1>
-          <h3 className="text-2xl text-green-700 p-2 mx-3">
-            곤약젤리 복숭아2개 특별가 5천원
+        <div className=" bg-pink-100 rounded-full">
+          <h1 className="pt-5">해외 공동구매 안내</h1>
+          <h3 className="text-2xl text-green-700 p-2 mx-3 my-0">
+            2023년 3월 : 곤약젤리 3개 5000원
           </h3>
-          <p className="font-semibold mb-2">
-            공동구매품목: 곤약젤리 12개 + 동전파스 20개
-          </p>
-          <div className="jap__info  p-3 text-left mb-2">
-            <div>
-              <li>- 물건부터 받는 해외주문 서비스 입니다</li>
-              <li>
-                - 추가 비용 없습니다. 단, 일주일간 다른 해외주문을 하시면 절대
-                안됩니다
-              </li>
-              <li>
-                - 깡통시장 제품을 먼저 보내 드리며, 평균 다음날 도착 합니다.
-              </li>
-              <li>- 나중에(일주일 전후) 통관되는 물건은 저희 소유가 됩니다</li>
-              <li>- 고객님의 개인통관고유번호로 공동구매 품목을 통관 합니다</li>
-              <li>- 대신 고객님께는 특별가 + 운임무료 혜택을 드립니다</li>
-              <li>- 다른 깡통 제품을 추가 하셔도 기본 택배비는 무료 입니다</li>
-              <li>- 입금 안내 → 입금 확인 → 해외 주문 → 깡통제품 택배발송</li>
-            </div>
-            <div>
-              - 다음 주문은 물건이 저희쪽에 도착 후 가능합니다(7~10일 소요)
-            </div>
+          <p className="mb-3">컵타입 복숭아 1봉지 + 짜먹는 타입 2봉지(랜덤)</p>
+          <div className="font-semibold mb-2 p-2 text-xl bg-white">
+            :공동구매품목:<div>곤약젤리 12개 + 동전파스 20개</div>
           </div>
+          <div className="jap__info  p-3  mb-2 flex flex-col text-xl font-semibold ">
+            {arr_jap_order_info.map((r) => {
+              return (
+                <li key={r} className="md:ml-40">
+                  {r}
+                </li>
+              )
+            })}
+            {/* <li> 물건부터 받는 해외주문 서비스 입니다</li>
+            <li>
+              추가 비용 없습니다. 단, 일주일간 다른 해외주문을 하시면 절대
+              안됩니다
+            </li>
+            <li>깡통시장 제품을 먼저 보내 드리며, 평균 다음날 도착 합니다.</li>
+            <li>나중에(일주일 전후) 통관되는 물건은 저희 소유가 됩니다</li>
+            <li>고객님의 개인통관고유번호로 공동구매 품목을 통관 합니다</li>
+            <li>대신 고객님께는 특별가 + 운임무료 혜택을 드립니다</li>
+            <li>다른 깡통 제품을 추가 하셔도 기본 택배비는 무료 입니다</li>
+            <li>입금 안내 → 입금 확인 → 해외 주문 → 깡통제품 택배발송</li> */}
+          </div>
+          <div className="p-3">
+            다음 주문은 공동구매 제품 통관 후 가능합니다
+          </div>
+
+          <div className="pb-3">7~10일 후</div>
         </div>
         {user || icUser ? (
           <Jap09Form icUser={icUser} />
@@ -140,10 +150,15 @@ export default function Jap() {
         background-position: center;
         background-size: cover;
         max-height:max-content;
-        min-height:50vw;
-        width: 97.2vw;
+        // min-height:50vw;
+        // width: 97.2vw;
+        display: flex;
+
       }
-        .jap__info {
+        .jap__info>li {
+          list-style-type: decimal-leading-zero;
+          font-size: 1rem;
+
         }
 
       `}</style>
