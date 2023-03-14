ebca74b27acb38cf6fb910c16336c64f1ef96e0e
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2023-03-14 12:53:01 +0900

    vscode !con

---
+++ src/context/theme.js
@@ -0,0 +1,29 @@
+import {createContext, useState, useContext} from 'react';
+
+const ThemeContext = createContext()
+const ThemeUpdateContext = createContext()
+
+export function useTheme(){ return useContext(ThemeContext)}
+export function useThemeUpdate(){ return useContext(ThemeContext)}
+
+export function ThemeProvider({children}) {
+	const [Theme, setTheme] = useState('');
+	const [dman, setDman] = useState(false)
+	const toggleTheme = () => {
+		setTheme(prev => !prev)
+	}
+
+	return (
+		<ThemeContext.Provider value={theme}>
+			<ThemeUpdateContext.Provider value={toggleTheme}>
+				{children}
+			</ThemeUpdateContext.Provider>
+		</ThemeContext.Provider>
+	)
+/**
+ * 사용하는 곳에서
+ * import {useTheme, useThemeUpdate} from '' 하고
+ * const darkTheme = useTheme()
+ * const  toggleTheme = useThemeUpdate() 해주면 끝
+ */
+}
