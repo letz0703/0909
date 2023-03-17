0d9b31cbde7b5fb05258578ded3ff114c58277f4
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2023-03-17 15:13:10 +0900

    useLogger

--- src/hooks/use-logger.js
+++ src/hooks/use-logger.js
@@ -1,12 +1,7 @@
 import { useState, useEffect } from "react"
 
 export function useLogger() {
-  const [value, setValue] = useState()
-
-  $1
-
   useEffect(() => {
     console.log("value:", value)
   }, [value])
-  return { value, setValue }
 }
