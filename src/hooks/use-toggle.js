8d46204c6f43d03ac2fb82d2350a2668a399a701
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2023-03-17 16:26:05 +0900

    use-toggle

---
+++ src/hooks/use-toggle.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react"
+
+//function here
+
+export default function useToggle(initialValue) {
+  const [value, setValue] = useState(initialValue)
+
+  function toggleValue(value) {
+    // setValue(true)
+    setValue((currentValue) =>
+      typeof value === "boolean" ? value : !currentValue
+    )
+  }
+  return [value, toggleValue]
+}
--- src/pages/ShopHome.jsx
+++ src/pages/ShopHome.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { SearchContext } from "../App"
 import JorderQuery from "../components/jorder-special/jorder-special"
 import Products from "../components/Products"
@@ -7,16 +7,20 @@ import Slide from "../components/slide/slide"
 import { useAuthContext } from "../context/AuthContext"
 import { useJapitems } from "../hooks/use-japitems"
 import { useLogger } from "../hooks/use-logger"
+import useToggle from "../hooks/use-toggle"
 import Store from "./Store"
 
 export default function ShopHome() {
   const { user } = useAuthContext()
-  const { search } = useContext(SearchContext)
-  const [japitems, setJapitems] = useJapitems()
-  useEffect(() => {}, [user])
+  // const { search } = useContext(SearchContext)
+  // const [japitems, setJapitems] = useJapitems()
+  // const [isUser, toggleValue] = useToggle(false)
+  // useEffect(() => {
+  //   toggleValue()
+  // }, [user])
   return (
     <div>
-      {/* {useLogger(search)} */}
+      {/* {console.log("value:", isUser)} */}
       <Slide />
       {user && <Products />}
     </div>
