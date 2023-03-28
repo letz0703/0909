7f94b9ebea9e9d91884c4275c1d6da1c099536a6
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2023-03-28 17:33:01 +0900

    setLocalStorage

--- src/pages/ShopHome.jsx
+++ src/pages/ShopHome.jsx
@@ -1,9 +1,11 @@
 import { useContext, useEffect, useState } from "react"
+import { getRDB_user, getRDB_users } from "../api/firebase"
 // import { SearchContext } from "../App"
 // import JorderQuery from "../components/jorder-special/jorder-special"
 import Products from "../components/Products"
 // import SearchInput from "../components/search-input/search-input"
 import Slide from "../components/slide/slide"
+import { useAuthContext } from "../context/AuthContext"
 // import { useAuthContext } from "../context/AuthContext"
 // import { useJapitems } from "../hooks/use-japitems"
 // import { useLogger } from "../hooks/use-logger"
@@ -12,7 +14,21 @@ import Slide from "../components/slide/slide"
 // import Test from "./Test"
 
 export default function ShopHome() {
-  // const { user } = useAuthContext()
+  const { user, uid } = useAuthContext()
+
+  useEffect(() => {
+    user &&
+      localStorage.setItem(
+        "ic-user",
+        JSON.stringify({
+          id: uid,
+          name: user.displayName,
+          phoneNumber: user.phoneNumber,
+          deliveryTo: "받으실 곳",
+        })
+      )
+  }, [user])
+
   return (
     <div>
       <Slide className=".slide" />
