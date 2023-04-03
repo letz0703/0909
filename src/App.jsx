97c60711fc12ae01b7eedd13bb750bac90555341
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2023-04-03 12:18:46 +0900

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { Container } from "react-bootstrap"
-import { useEffect, useState } from "react"
-import { Routes, Route, Outlet } from "react-router-dom"
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import {Container} from "react-bootstrap"
+import {useEffect, useState} from "react"
+import {Routes, Route, Outlet} from "react-router-dom"
 import "./App.css"
 import Navbar from "./components/Navbar"
-import { AuthContextProvider } from "./context/AuthContext"
+import {AuthContextProvider} from "./context/AuthContext"
 import ShopHome from "./pages/ShopHome"
-import { ShoppingCartProvider } from "./context/ShoppingCartContext"
-import { v4 as uuidv4 } from "uuid"
-import { createContext } from "react"
-import { extraData } from "./data.js"
-import { collection, getDocs } from "firebase/firestore"
-import { db } from "./api/firebase"
+import {ShoppingCartProvider} from "./context/ShoppingCartContext"
+import {v4 as uuidv4} from "uuid"
+import {createContext} from "react"
+import {extraData} from "./data.js"
+import {collection, getDocs} from "firebase/firestore"
+import {db} from "./api/firebase"
 import SearchHeader from "./components/SearchHeader/SearchHeader"
 import styles from "./App2.module.css"
-import { YoutubeApiProvider } from "./context/YoutubeApi"
-import { DetailContextProvider } from "./context/DetailContext"
+import {YoutubeApiProvider} from "./context/YoutubeApi"
+import {DetailContextProvider} from "./context/DetailContext"
 import Detail from "./components/Detail"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
 
 const queryClient = new QueryClient()
 export const JapitemContext = createContext()
@@ -47,25 +47,25 @@ function App() {
     setSearch(e.target.value)
   }
 
-  const searchContextValue = { handleSearch, search }
+  const searchContextValue = {handleSearch, search}
 
   function handleJapitemAdd() {
     const newJapitem = {
       id: uuidv4(),
       name: "new item",
-      price: 10000,
+      price: 10000
     }
     setJapitems([...japitems, newJapitem])
   }
 
   function handleJapitemDelete(id) {
-    setJapitems(japitems.filter((japitem) => japitem.id !== id))
+    setJapitems(japitems.filter(japitem => japitem.id !== id))
   }
 
   const japitemContextValue = {
     handleJapitemAdd,
     handleJapitemDelete,
-    japitems,
+    japitems
   }
   if (location.pathname !== "/videos") {
     return (
@@ -111,13 +111,13 @@ const sampleJapitems = [
     code: 1234,
     name: "샤론파스 80매",
     price: 10000,
-    imgUrl: "/imgs/shron80.jpg",
+    imgUrl: "/imgs/shron80.jpg"
   },
   {
     id: uuidv4(),
     code: 5678,
     name: "동전파스 156매 2각",
     price: 10000,
-    imgUrl: "/imgs/donjunpas156.jpg",
-  },
+    imgUrl: "/imgs/donjunpas156.jpg"
+  }
 ]
