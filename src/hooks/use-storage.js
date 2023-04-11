f8a33ac379d34405f848457cb1dfe9a4edafa4c4
ManI <letz0703@gmail.com>
2023-04-11 12:43:30 +0900

    Create use-storage.js

---
+++ src/hooks/use-storage.js
@@ -0,0 +1,30 @@
+export function useSessionStorage(key, defaultValue) {
+  return useStorage(key, defaultValue, window.sessionStorage)
+}
+export function useLocalStorage(key, defaultValue) {
+  return useStorage(key, defaultValue, window.localStorage)
+}
+
+const useStorage = (key, defaultValue, storageObject) => {
+  const [value, setValue] = useState(() => {
+    const jsonValue = storageObject.getItem(key)
+    if (jsonValue != null) return JSON.parse(jsonValue)
+
+    if (typeof initialValue === "function") {
+      return defaultValue()
+    } else {
+      return defaultValue
+    }
+  })
+
+  useEffect(() => {
+    if (value === undefined) return storageObject.removeItem(key)
+    storageObject.setItem(key, JSON.stringify(value))
+  }, [key, value, storageObject])
+
+  const remove = useCallback(()=>{
+    setValue(undefined)
+  }, [])
+
+  return [value, setValue, remove]
+}
