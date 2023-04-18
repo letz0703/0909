f8faffd3d01e3f473244c4700057218ddc31b2a8
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2023-04-18 13:50:13 +0900

    削除 delete

--- src/components/jap09-form/Agree.jsx
+++ src/components/jap09-form/Agree.jsx
@@ -1,12 +1,17 @@
 import React from "react"
+import { useEffect } from "react"
 //import styles from './Agree.module.css'
 
-export function Agree({ isComplete }) {
+export function Agree({ deleteRequest, handleDeleteRequest }) {
   return (
     <>
-      <div>모두 확인 하였습니다</div>
+      <div>통관 후 삭제 요청 체크</div>
       <label>
-        <input type="checkbox" defaultChecked={isComplete} />
+        <input
+          type="checkbox"
+          defaultChecked={deleteRequest}
+          onClick={handleDeleteRequest}
+        />
       </label>
     </>
   )
--- src/components/jap09-form/custom_info.jsx
+++ src/components/jap09-form/custom_info.jsx
@@ -1,9 +1,21 @@
 import { useState } from "react"
 import FormWrapper from "./form-wrapper"
 import { Agree } from "./Agree"
+import { useRef } from "react"
+import { useEffect } from "react"
 
 export default function CustomInfo() {
   let SpecialSelected = "special-selected.jsx"
+
+  const [deleteRequest, setDeleteRequest] = useState(false)
+
+  function handleDeleteRequest() {
+    setDeleteRequest((prev) => !prev)
+  }
+
+  useEffect(() => {
+    localStorage.setItem("deleteRequest", JSON.stringify(deleteRequest))
+  }, [deleteRequest])
 
   return (
     <div className="bg-blue-100 p-4 rounded-b-xl">
@@ -17,8 +29,10 @@ export default function CustomInfo() {
       <p>공동구매 후 동의서를 유지하시려면 아래에 체크 하세요</p>
       <p>체크가 없을 경우 개인통관 번호은 통관 1개월 내 삭제 하며</p>
       <p>재주문 시 다시 동의 신청 하셔야 합니다</p>
-      <Agree isComplete />
-      {/* <div className="bg-blue-200 p-2">기타 문의는 twitter로 트윗해주세요</div> */}
+      <Agree
+        deleteRequest={deleteRequest}
+        handleDeleteRequest={handleDeleteRequest}
+      />
 
       <style>{`
         .special-selected {
