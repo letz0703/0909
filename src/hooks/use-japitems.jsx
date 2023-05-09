3ec019a0954852ec42e2bd025cfe5ac3801e3e97
ManI <letz0703@gmail.com>
2023-05-10 04:55:38 +0900

    로딩 from 로컬 product

--- src/hooks/use-japitems.jsx
+++ src/hooks/use-japitems.jsx
@@ -5,9 +5,19 @@ import { useLocalStorage } from "../hooks/use-local-storage"
 import { useState } from "react"
 
 export const useJapitems = () => {
+  const [isLoading, setIsLoading] = useState(true)
   const [japitems, setJapitems] = useLocalStorage("japitems", [])
 
   useEffect(() => {
+    setIsLoading(true)
+
+    const controller = new AbortController()
+    fetch("products.json", {
+      signal: controller.signal,
+    })
+      .then((response) => response.json())
+      .then(setJapitems)
+      .finally(() => setIsLoading(false))
     get(ref(database, "japitems")).then((snapshot) => {
       if (snapshot.exists()) {
         setJapitems(Object.values(snapshot.val()))
