47198074d86b79041c02a51a0c7f18ca076c02ea
ManI <letz0703@gmail.com>
2023-05-11 23:36:55 +0900

    form 수정

--- src/components/jap09-form/Agree.jsx
+++ src/components/jap09-form/Agree.jsx
@@ -1,12 +1,11 @@
 import React from "react"
 import { useEffect } from "react"
-//import styles from './Agree.module.css'
 
 export function Agree({ deleteRequest, handleDeleteRequest }) {
   return (
     <>
-      <div>통관 후 삭제 요청 체크</div>
-      <label>
+      <span className="">통관 후 삭제 요청 체크</span>
+      <label className="w-auto">
         <input
           type="checkbox"
           defaultChecked={deleteRequest}
