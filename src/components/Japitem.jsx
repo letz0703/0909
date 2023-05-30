0c378b31776f14cbfef6c5a43b2a770ad33ab451
ManI <letz0703@gmail.com>
2023-05-30 20:08:47 +0900

    gepi

--- src/components/Japitem.jsx
+++ src/components/Japitem.jsx
@@ -11,34 +11,47 @@ export function Japitem({ japitem }) {
   const navigate = useNavigate()
 
   return (
-    <div className={`${styles.product}  flex flex-col items-center card `}>
-      <span className="max-w-[80%] truncate">{japitem.name}</span>
-      <span className="text-orange-500 font-bold">
-        {FormatCurrency(japitem.price)}
-      </span>
-      <span className="transition-all hover:scale-110 mt-auto ">
-        <img
-          src={japitem.imgUrl}
-          className="max-h-[150px]"
-          onClick={() => {
-            navigate(`/japitems/${japitem.id}`, { state: { japitem } })
-          }}
-        />
-      </span>
+    <div
+      className={`${styles.product}  flex flex-col items-center card `}
+      style={{ heigh: "100%" }}
+    >
+      <div
+        className="wrapper"
+        style={{
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <span className="max-w-[80%] truncate">{japitem.name}</span>
+        <span className="text-orange-500 font-bold">
+          {FormatCurrency(japitem.price)}
+        </span>
+        <span className="transition-all hover:scale-110 mt-auto ">
+          <img
+            src={japitem.imgUrl}
+            className="max-h-[150px]"
+            onClick={() => {
+              navigate(`/japitems/${japitem.id}`, { state: { japitem } })
+            }}
+          />
+        </span>
 
-      {japitem.qty <= 0 ? (
-        <button className="btn btn--danger mini text-xs ">품절</button>
-      ) : (
-        <button
-          className="btn btn--primary mini text-xs"
-          onClick={() => {
-            !user && login()
-            increaseCartQuantity(japitem.id)
-          }}
-        >
-          담기
-        </button>
-      )}
+        {japitem.qty <= 0 ? (
+          <button className="btn btn--danger mini text-xs ">품절</button>
+        ) : (
+          <button
+            className="btn btn--primary mini text-xs"
+            onClick={() => {
+              !user && login()
+              increaseCartQuantity(japitem.id)
+            }}
+          >
+            담기
+          </button>
+        )}
+      </div>
     </div>
   )
 }
