33b04afb075b7afd477280d913ea870fbcbc5e4a
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2023-06-05 15:30:17 +0900

    working at m2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,20 +4,8 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-
   theme: {
-    extend: {
-      aspectRatio:{'16/9':'16/9', '4/3':'4/3'},
-      colors: {
-        youtube: '#ff0000',
-        brand:'#c10002'
-      },
-      backgroundImage: {
-        banner: `url('../public/imgs/banner.jpg' )`,
-      }
-    }
+    extend: {},
   },
-  plugins: [
-    require('@tailwindcss/line-clamp')
-  ]
+  plugins: [],
 }
