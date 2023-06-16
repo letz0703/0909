c5e1e30610918d1bd93d2e204c3e2593059dc2f3
ManI <letz0703@gmail.com>
2023-06-17 04:28:56 +0900

    ...

--- src/pages/Info_09.jsx
+++ src/pages/Info_09.jsx
@@ -6,9 +6,9 @@ export function Info09() {
   return [
     //prettier-ignore
     <section style={{width:"maxContent"}}>
-        <h3 className={`text-[3rem] font-bold `}>wizbox</h3>
-        <p>010.9876.1815</p>
-        <p className={`text-[1.0rem] text-[#c10002]`}>.shop by i.canmart(인터넷 깡통시장)</p>
+        <h3 className={`text-[3rem]  `}>wizbox</h3>
+        <p className={`text-[1.1rem] text-[#c10002]`}>.shop <br/>by <br/>i.canmart</p>
+        <div className={`pt-1 text-xl pt-2`}>010.9876.1815</div>
         <div className={`flex justify-center align-center`}>
         {/*<pre className={`w-auto text-[1.4em] py-2 `}>다음날 받는 해외주문</pre>*/}
         </div>
@@ -47,13 +47,13 @@ export function Info09() {
         </p>*/}
         <div>
           <span className={`inline-block`}>
-            문자와 트위터만 확인 합니다.
+            문자와 트윗만 확인 합니다.
             <FaSms color="#c10002" className={`inline-block mr-2  `} size={50} />
           </span>
         </div>
-        <div className={`text-slate-700`}>본 사이트는 저희 단골고객님과 <br/>오프라인 가게 방문 고객님들 중 소수분들께만 알려드리는<br/>
+        <div className={`text-slate-700 pt-3`}>본 사이트는 저희 단골고객님과 <br/>오프라인 가게 방문 고객님들 <br/>중 소수분들께만 알려드리는<br/>
         서비스 사이트 입니다</div>
-        <div className={`text-2xl pt-3 font-bold`}>밤 7시 ~ 새벽 7시 까지만 open 합니다</div>
+        <div className={`text-xl pt-3 font-bold`}>밤 7시 ~ 새벽 7시까지만 open 합니다.</div>
         <div>
           🛍️ 주문 하시려면
           <button onClick={login} className={`btn btn--primary  `}>
