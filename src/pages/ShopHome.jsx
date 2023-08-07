f950aad93d43e6012025083a682d930f0967f801
ManI <letz0703@gmail.com>
2023-08-07 22:16:51 +0900

    Update ShopHome.jsx

--- src/pages/ShopHome.jsx
+++ src/pages/ShopHome.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable no-unused-vars */
 
-import { useContext, useEffect, useRef, useState } from "react"
-import { getRDB_user, getRDB_users } from "../api/firebase"
+import { useEffect, useState } from "react"
+//import { useContext, useEffect, useRef, useState } from "react"
+//import { getRDB_user, getRDB_users } from "../api/firebase"
 // import { SearchContext } from "../App"
 // import JorderQuery from "../components/jorder-special/jorder-special"
 import Products from "../components/Products"
 // import SearchInput from "../components/search-input/search-input"
-import Slide from "../components/slide/slide"
+//import Slide from "../components/slide/slide"
 import { useAuthContext } from "../context/AuthContext"
-import useUpdatedEffect from "../hooks/use-updated-effect"
+//import useUpdatedEffect from "../hooks/use-updated-effect"
 // import { useAuthContext } from "../context/AuthContext"
 // import { useJapitems } from "../hooks/use-japitems"
 // import { useLogger } from "../hooks/use-logger"
@@ -17,22 +18,22 @@ import useUpdatedEffect from "../hooks/use-updated-effect"
 // import Test from "./Test"
 import product from "./data_product.json"
 import styles from "./ShopHome.module.css"
-import Navbar from "../components/Navbar"
+//import Navbar from "../components/Navbar"
 //import { Gallery } from "../components/Gallery/Gallery"
-import { Easy } from "../components/Easy/Easy"
+//import { Easy } from "../components/Easy/Easy"
 import { Intro } from "./Intro"
 import { Intro2 } from "./Intro2"
-import { Wizbox } from "./Wizbox"
+//import { Wizbox } from "./Wizbox"
 import { Info09 } from "./Info_09"
-import { UseOpenClose } from "../util/use_open_close"
-import useArray from "../hooks/use-array"
+//import { UseOpenClose } from "../util/use_open_close"
+//import useArray from "../hooks/use-array"
 
-const INITIAL_ARRAY = ["am", "pm"]
+//const INITIAL_ARRAY = ["am", "pm"]
 
 export default function ShopHome() {
   const { user, uid, login, logout } = useAuthContext()
-  const timeOpen = 19 * 60 * 60 - 300 //6:56 PM
-  const timeClose = 7 * 60 * 60 // 7:00 AM
+  //const timeOpen = 19 * 60 * 60 - 300 //6:56 PM
+  //const timeClose = 7 * 60 * 60 // 7:00 AM
   const [html_open, setHtml_open] = useState(true)
   //const { array, set } = useArray(INITIAL_ARRAY)
 
