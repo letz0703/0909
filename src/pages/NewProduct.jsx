c0eaf16a258e0a3eda89aa7abb2a4e71d5d9a57f
ManI <letz0703@gmail.com>
2023-08-16 04:32:04 +0900

    saved name

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,36 +1,49 @@
 // import { useAuthContext } from "../../context/AuthContext"
 import { v4 as uuidv4 } from "uuid"
+import "./NewProduct.css"
 import { getOrders, database, db } from "../api/firebase"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import FormatCurrency from "../util/formatCurrency"
 import { uploadImage } from "../api/uploader"
 import { useJapitems } from "../hooks/use-japitems"
-import { getDatabase, ref, set, get, remove, child } from "firebase/database"
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+  remove,
+  child,
+  update,
+} from "firebase/database"
 import { useLocalStorage } from "../hooks/useLocalStorage"
 import IcORders from "../components/IcORders"
 import CSVtoJSONConverter from "../util/converter_json"
 
 export default function NewProduct() {
-  const [newProduct, setNewProduct] = useState([])
-  const [addedJapitem, setAddedJapitem] = useState([])
+  const [newProduct, setNewProduct] = useState({})
+  const refName = useRef("")
 
+  const handleJapitemDelete = async (id) => {
+    remove(ref(database, `japitems/${id}`))
+      .then(() => console.log("data deleted"))
+      .catch((error) => alert("error", error))
+  }
   function updateFields(fields) {
     setNewProduct((prev) => {
       return { ...prev, ...fields }
     })
   }
+  const [japitems, setJapitems] = useJapitems()
 
-  const createJapitem = async (data) => {
-    const id = crypto.randomUUID()
-    set(ref(database, `japitems/${id}`), {
-      id: id,
-      code: data.code || "",
+  const createJapitem = (data) => {
+    update(ref(database, `japitems/${data.id}`), {
+      //id: data.id,
       name: data.name,
-      description: data.description,
-      price: data.price,
-      imgUrl: data.imgUrl,
-      homeUrl: data.homeUrl || "",
-      qty: Number(data.qty) || 0,
+      //price: data.price,
+      //qty: Number(data.qty) || 0,
+      //description: data.description,
+      //imgUrl: data.imgUrl,
+      //homeUrl: data.homeUrl || "",
     })
       .then(() => {
         alert("data saved")
@@ -38,113 +51,38 @@ export default function NewProduct() {
       .catch((error) => console.log(error))
   }
 
-  const handleJapitemDelete = async (id) => {
-    remove(ref(database, `japitems/${id}`))
-      .then(() => console.log("data deleted"))
-      .catch((error) => alert("error", error))
-  }
-
-  function onSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault()
+    //if (e.target.value == "") return
+    setNewProduct({ id: crypto.randomUUID(), name: refName.current.value })
     createJapitem(newProduct)
-    setAddedJapitem(newProduct)
+    //createJapitem(newProduct)
+    //setAddedJapitem(newProduct)
   }
-
+  //function handleSubmit() {
+  //console.log(newProduct)
+  //}
   return (
     <div className={`pt-[6rem]`}>
-      <div>
-        <label htmlFor="searchOrder">Oder Search</label>
-        <input type="text" id="searchOrder" />
-      </div>
-
       <CSVtoJSONConverter />
 
       <div className="new-product__Main  w-full text-center flex flex-col justify-center items-center">
-        <div className="new-product__list place-content-center text-center w-full">
-          <span>code: {addedJapitem.code}</span>
-          <span>
-            {addedJapitem.name}
-            <button
-              onClick={() => {
-                handleJapitemDelete(addedJapitem.id)
-              }}
-              className="btn btn--danger mini"
-            >
-              del
-            </button>
-          </span>
-          <span className="text-orange-500 font-bold">
-            {FormatCurrency(addedJapitem.price)}
-          </span>
-          <img
-            src={addedJapitem?.imgUrl || null}
-            className="new-product__list-image mx-auto"
-            style={{ width: "96px" }}
-          />
-          <span>&times;</span>
-          <span>
-            {addedJapitem.qty}개{" "}
-            <button
-              onClick={() => {
-                handleUpdateStock(addedJapitem.id, addedJapitem.qty)
-              }}
-              className="btn mini btn--primary"
-            >
-              +
-            </button>
-            <span className="text-xs text-gray-500">in qty</span>
-          </span>
-        </div>
-        <span className="font-semibold">
-          TOTAL: {FormatCurrency(addedJapitem.qty * addedJapitem.price)}
-        </span>
-
         <h1>전체 주문</h1>
         <IcORders />
-
-        <style>{`
-        .newProduct__table{
-          // border-collapse: collapse;
-          // text-align: left;
-          // line-height: 1.5;
-          // margin : 20px 10px;
-        }
-
-          .new-product__Main input {
-            background-color: #c10002;
-            padding: 1.2em;
-            color: #e1e1e1;
-          }
-          .new-product__Main {
-            display: grid;
-          }
-          .new-product__form-group{
-            display: grid;
-            gap: 0.3rem;
-          }
-          .new-product__form-group--full-width{
-            grid-column: 1/ -1;
-          }
-          .new-product__form {
-            display: grid;
-            gap: 1.25rem;
-          }
-          @media (min-width: 1024px) {
-            .new-product__form {
-              grid-template-columns: repeat(6, 1fr);
-            }
-          }
-          .new-product__list{
-            display: grid;
-            grid-template-columns: repeat(6, 12rem);
-            border-bottom: 1px dashed black;
-            padding: 2rem;
-          }
-          .new-product__list-image{
-            width: 10rem;
-          }
-          `}</style>
       </div>
+      <section>
+        <h3>Add New Item</h3>
+        <form id="new-product-form" onSubmit={handleSubmit}>
+          <label htmlFor="name">new item</label>
+          <input
+            type="text"
+            id="name"
+            ref={refName}
+            value={refName.current.value}
+          />
+          <button className={`btn btn--primary`}>add</button>
+        </form>
+      </section>
     </div>
   )
 }
