cddb4cb068a89c55f5f275ca99c206326dcb5b9c
ManI <letz0703@gmail.com>
2023-08-27 23:56:55 +0900

    paint stroke 2023.08.27/일

--- src/pages/Info_09.jsx
+++ src/pages/Info_09.jsx
@@ -8,12 +8,16 @@ export function Info09() {
       <section key="info09" style={{ width: "maxContent" }}>
         <h3 className={`text-[3rem]  pb-[0.9rem]`}>wizbox</h3>
         <p className={`text-[1.1rem] text-[#c10002]`}>
-          <span className={`text-[3rem]`}>.</span>
+          {/*<span className={`text-[3rem]`}>.</span>*/}
           <br />
-          shop <br />
-          by <br />
-          i.canmart
+          🛍 <br />
+          japan
         </p>
+        <div className="relative">
+          <div class=" absolute right-[50%] w-[40px] translate-x-[130px] -translate-y-2">
+            <img src="/public/stroke_red.png" alt="stroke" />
+          </div>
+        </div>
         <div className={`pt-1 text-xl pb-1`}>010.9876.1815</div>
         {/*<div className={`flex justify-center align-center`}>*/}
         {/*<pre className={`w-auto text-[1.4em] py-2 `}>다음날 받는 해외주문</pre>*/}
@@ -41,7 +45,7 @@ export function Info09() {
           본 사이트는 저희 단골고객과 <br />
           오프라인 가게 방문 고객분들
           <br />
-          께만 알려드리고 있습니다.
+          께만 알려드리는 깡통시장
           <br />
           가격정보 사이트 입니다.
         </div>
@@ -51,6 +55,7 @@ export function Info09() {
         <div className={`text-xl pt-[1em] font-semibold text-gray-500`}>
           {/*오프라인 깡통시장 가격 보다 1% 더 저렴합니다*/}
         </div>
+
         <div>
           {/*🛍️ 주문 하시려면*/}
           <button onClick={login} className={`btn btn--primary  `}>
