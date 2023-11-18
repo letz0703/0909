362ba8e75c4fad9687795c8624efc3cd2050d05b
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2023-11-18 17:24:08 +0900

    2023.11.18/토

--- src/pages/NewTodo.jsx
+++ src/pages/NewTodo.jsx
@@ -1,23 +1,25 @@
-import { Link } from "react-router-dom"
+import { Form, Link } from "react-router-dom"
 
 export function NewTodo() {
+  const errorMessage = use()
   return (
     <div className={`container`}>
       <h1 className="page-title">New Todo</h1>
-      <form className={`form`}>
+      <Form className={`form`} method="post">
+        <div>{errorMessage}</div>
         <div className="form-row">
           <div className="form-group">
             <label htmlFor="title">title</label>
             <input type="text" id="title" name="title" />
           </div>
-          <div className="form-btn-row form-row">
-            <Link to=".." className={`btn btn-outline`}>
-              Back
-            </Link>
-            <div className="btn">Create</div>
-          </div>
         </div>
-      </form>
+        <div className="form-btn-row form-row">
+          <Link to=".." className={`btn btn-outline`}>
+            Back
+          </Link>
+          <button className="btn">Create</button>
+        </div>
+      </Form>
     </div>
   )
 }
