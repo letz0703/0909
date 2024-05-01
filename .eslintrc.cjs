7b3169b1a779a2f23d81874100e8ea374a8785a7
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2024-05-01 13:24:44 +0900

    eslint.cjs update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    "esling:recommended",
+    "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
